05c7bdc1f085df7409040008068a17f198b4cc93
Veena Mudda <110976068+VeenaMudda@users.noreply.github.com>
2023-04-24 19:24:35 +0530

    Create index.js

---
+++ src/components/Contact/About/index.js
@@ -0,0 +1,16 @@
+// Write your JS code here
+import './index.css'
+
+const About = () => (
+  <div className="about-container">
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/about-blog-img.png"
+      alt="about"
+      className="image"
+    />
+    <h1 className="heading">About</h1>
+    <p className="content">I love to create! I am a frontend web developer</p>
+  </div>
+)
+
+export default About
